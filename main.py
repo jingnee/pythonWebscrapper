3316b637a1f98528988e155b57e5f996fa141527
chojy2 <chojy2@brandi.co.kr>
2020-12-29 22:39:44 +0900

    extract {title, company, location, apply link}

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from indeed import extract_indeed_pages, extract_indeed_jobs 
 #wanted_result = requests.get("https://www.wanted.co.kr/wdlist/518/674?country=kr&job_sort=job.latest_order&years=-1&locations=all") 
-last_indeed_pages = extract_indeed_pages() 
-extract_indeed_jobs(last_indeed_pages)
+last_indeed_pages = extract_indeed_pages()
+jobs = extract_indeed_jobs(last_indeed_pages)
+
+print(jobs)
