c025283ce50db4d3a3b750b090f38c0e6ede6e04
cho jiyun <jingnee721@gmail.com>
2020-12-29 22:58:22 +0900

    done indeed, add stackoverflow file(empty)

--- indeed.py
+++ indeed.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 LIMIT = 50
 URL = f"https://www.indeed.com/jobs?q=python&limit={LIMIT}"
-def extract_indeed_pages():
+def get_last_page():
     result = requests.get(URL)
     soup = BeautifulSoup(result.text, 'html.parser')
     pagination = soup.find("div", {"class": "pagination"})
@@ -34,7 +34,7 @@ def extract_job(html):
         "link":f"https://www.indeed.com/viewjob?jk={job_id}"
     }
 
-def extract_indeed_jobs(last_pages):
+def extract_jobs(last_pages):
     jobs = []
     for page in range(last_pages):
         print(f"Scrapping page {page}")
@@ -45,4 +45,9 @@ def extract_indeed_jobs(last_pages):
         for result in results:
             job = extract_job(result)
             jobs.append(job)
+    return jobs
+
+def get_jobs():
+    last_page = get_last_page()
+    jobs = extract_jobs(last_page)
     return jobs
