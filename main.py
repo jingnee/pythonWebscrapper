9eeffd238f9e2c972d51990b39cb72291520cf6e
chojy2 <chojy2@brandi.co.kr>
2021-01-02 19:12:56 +0900

    extract {title, company, location, apply link}

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from indeed import get_jobs as get_indeed_jobs
+from stackoverflow import get_jobs as get_so_jobs
 #wanted_result = requests.get("https://www.wanted.co.kr/wdlist/518/674?country=kr&job_sort=job.latest_order&years=-1&locations=all") 
 
-indeed_jobs = get_indeed_jobs()
-
-print(indeed_jobs)
+#indeed_jobs = get_indeed_jobs()
+#print(indeed_jobs)
+so_jobs = get_so_jobs()
